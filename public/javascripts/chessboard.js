831ebe153e5f2a05a50be3f9b5dac60cda1ecc3c
Owner <alex@ubuntu.(none)>
2014-09-12 01:13:21 -0400

    test

---
+++ public/javascripts/chessboard.js
@@ -0,0 +1,42 @@
+	$(document).ready(function(){
+
+	$('.row:even > .space:odd').css('background-color', '#555555');
+	$('.row:odd > .space:even').css('background-color', '#555555');
+   $('#0').children('.piece').children('img').attr('src', 'images/black_rook.png');
+   $('#1').children('.piece').children('img').attr('src', 'images/black_knight.png');
+   $('#2').children('.piece').children('img').attr('src', 'images/black_bishop.png');
+   $('#3').children('.piece').children('img').attr('src', 'images/black_queen.png');
+   $('#4').children('.piece').children('img').attr('src', 'images/black_king.png');
+   $('#5').children('.piece').children('img').attr('src', 'images/black_bishop.png');
+   $('#6').children('.piece').children('img').attr('src', 'images/black_knight.png');
+   $('#7').children('.piece').children('img').attr('src', 'images/black_rook.png');
+   
+   $('#8').children('.piece').children('img').attr('src', 'images/black_pawn.png');
+   $('#9').children('.piece').children('img').attr('src', 'images/black_pawn.png');
+   $('#10').children('.piece').children('img').attr('src', 'images/black_pawn.png');
+   $('#11').children('.piece').children('img').attr('src', 'images/black_pawn.png');
+   $('#12').children('.piece').children('img').attr('src', 'images/black_pawn.png');
+   $('#13').children('.piece').children('img').attr('src', 'images/black_pawn.png');
+   $('#14').children('.piece').children('img').attr('src', 'images/black_pawn.png');
+   $('#15').children('.piece').children('img').attr('src', 'images/black_pawn.png');
+   
+   
+   $('#56').children('.piece').children('img').attr('src', 'images/white_rook.png');
+   $('#57').children('.piece').children('img').attr('src', 'images/white_knight.png');
+   $('#58').children('.piece').children('img').attr('src', 'images/white_bishop.png');
+   $('#59').children('.piece').children('img').attr('src', 'images/white_queen.png');
+   $('#60').children('.piece').children('img').attr('src', 'images/white_king.png');
+   $('#61').children('.piece').children('img').attr('src', 'images/white_bishop.png');
+   $('#62').children('.piece').children('img').attr('src', 'images/white_knight.png');
+   $('#63').children('.piece').children('img').attr('src', 'images/white_rook.png');
+   
+   $('#48').children('.piece').children('img').attr('src', 'images/white_pawn.png');
+   $('#49').children('.piece').children('img').attr('src', 'images/white_pawn.png');
+   $('#50').children('.piece').children('img').attr('src', 'images/white_pawn.png');
+   $('#51').children('.piece').children('img').attr('src', 'images/white_pawn.png');
+   $('#52').children('.piece').children('img').attr('src', 'images/white_pawn.png');
+   $('#53').children('.piece').children('img').attr('src', 'images/white_pawn.png');
+   $('#54').children('.piece').children('img').attr('src', 'images/white_pawn.png');
+   $('#55').children('.piece').children('img').attr('src', 'images/white_pawn.png');
+	
+	});
